c08bb485fb717538291b475d5e8ee2f3c8eb8140
tgandor <tomasz.gandor@gmail.com>
2021-01-12 02:52:15 +0100

    validate_coco.py - for R50 on Q=1-100.

--- validate_coco.py
+++ validate_coco.py
@@ -2,6 +2,9 @@ import argparse
 import logging
 import os
 import time
+import json
+
+import torch
 
 import detectron2.model_zoo
 from detectron2.config import get_cfg
@@ -18,14 +21,30 @@ if not os.path.exists('datasets'):
     print('Copy or symlink datasets/ directory here. (please)')
     exit()
 
-cfg = get_cfg()
-cfg.merge_from_file(detectron2.model_zoo.get_config_file(model_config))
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
-cfg.MODEL.WEIGHTS = detectron2.model_zoo.get_checkpoint_url(model_config)
-predictor = DefaultPredictor(cfg)
-data_loader = build_detection_test_loader(cfg, 'coco_2017_val')
-evaluator = COCOEvaluator('coco_2017_val', cfg, False, 'evaluator_dump')
-start = time.time()
-results = inference_on_dataset(predictor.model, data_loader, evaluator)
-inference_time = time.time() - start
-print(results)
+if not os.path.exists('val2017.zip'):
+    print('Copy or symlink val2017.zip file here. (please)')
+    exit()
+
+def validate_quality(q, model='R50'):
+    out_folder = f'evaluator_dump_{model}_{q:03d}'
+    os.system('unzip -o val2017.zip -d datasets/coco/')
+    os.system(f'mogrify -verbose -quality {q} datasets/coco/val2017/*')
+    cfg = get_cfg()
+    cfg.merge_from_file(detectron2.model_zoo.get_config_file(model_config))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+    cfg.MODEL.WEIGHTS = detectron2.model_zoo.get_checkpoint_url(model_config)
+    predictor = DefaultPredictor(cfg)
+    data_loader = build_detection_test_loader(cfg, 'coco_2017_val')
+    evaluator = COCOEvaluator('coco_2017_val', cfg, False, out_folder)
+    start = time.time()
+    results = inference_on_dataset(predictor.model, data_loader, evaluator)
+    inference_time = time.time() - start
+    print(results)
+    torch.save(results, out_folder + '/results.pth')
+    with open(out_folder + '/results.json') as jsf:
+        json.dump({'quality': q, 'model': model, 'results': results, 'elapsed': inference_time}, jsf)
+
+
+if __name__ == '__main__':
+    for i in range(1, 101):
+        validate_quality(i)
