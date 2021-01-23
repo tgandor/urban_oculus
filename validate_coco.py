0392d882ccb4f3b4ab3753c0c48c65945bf8b003
tgandor <tomasz.gandor@gmail.com>
2021-01-24 00:35:53 +0100

    summarize_model.ipynb - and model representations in model_dumps/. Reformatted validate_coco.py.

--- validate_coco.py
+++ validate_coco.py
@@ -27,53 +27,61 @@ MODEL_ZOO_CONFIGS = {
     "R101": "COCO-Detection/retinanet_R_101_FPN_3x.yaml",
 }
 
-if not os.path.exists('datasets'):
-    print('Copy or symlink datasets/ directory here. (please)')
+if not os.path.exists("datasets"):
+    print("Copy or symlink datasets/ directory here. (please)")
     exit()
 
-if not os.path.exists('val2017.zip'):
-    print('Copy or symlink val2017.zip file here. (please)')
+if not os.path.exists("val2017.zip"):
+    print("Copy or symlink val2017.zip file here. (please)")
     exit()
 
-def validate_quality(q, model='R50'):
+
+def validate_quality(q, model="R50"):
     model_config = MODEL_ZOO_CONFIGS[model]
-    out_folder = f'evaluator_dump_{model}_{q:03d}'
-    os.system('unzip -o val2017.zip -d datasets/coco/')
-    os.system(f'mogrify -verbose -quality {q} datasets/coco/val2017/*')
+    out_folder = f"evaluator_dump_{model}_{q:03d}"
+    os.system("unzip -o val2017.zip -d datasets/coco/")
+    os.system(f"mogrify -verbose -quality {q} datasets/coco/val2017/*")
     cfg = get_cfg()
     cfg.merge_from_file(detectron2.model_zoo.get_config_file(model_config))
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
     cfg.MODEL.WEIGHTS = detectron2.model_zoo.get_checkpoint_url(model_config)
     predictor = DefaultPredictor(cfg)
-    data_loader = build_detection_test_loader(cfg, 'coco_2017_val')
-    evaluator = COCOEvaluator('coco_2017_val', cfg, False, out_folder)
+    data_loader = build_detection_test_loader(cfg, "coco_2017_val")
+    evaluator = COCOEvaluator("coco_2017_val", cfg, False, out_folder)
     start = time.time()
     results = inference_on_dataset(predictor.model, data_loader, evaluator)
     inference_time = time.time() - start
     print(results)
-    torch.save(results, out_folder + '/results.pth')
-    with open(out_folder + '/results.json', 'w') as jsf:
-        json.dump({
-            'quality': q,
-            'model': model,
-            'results': results,
-            'elapsed': inference_time,
-            'device': torch.cuda.get_device_name(),
-        }, jsf)
+    torch.save(results, out_folder + "/results.pth")
+    with open(out_folder + "/results.json", "w") as jsf:
+        json.dump(
+            {
+                "quality": q,
+                "model": model,
+                "results": results,
+                "elapsed": inference_time,
+                "device": torch.cuda.get_device_name(),
+            },
+            jsf,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('model')
-    parser.add_argument('--device', '-d', type=int, help='select CUDA device')
-    parser.add_argument('--minQ', type=int, help='min JPEG quality', default=1)
-    parser.add_argument('--maxQ', type=int, help='min JPEG quality', default=100)
+    parser.add_argument("model")
+    parser.add_argument("--device", "-d", type=int, help="select CUDA device")
+    parser.add_argument("--minQ", type=int, help="min JPEG quality", default=1)
+    parser.add_argument("--maxQ", type=int, help="min JPEG quality", default=100)
     args = parser.parse_args()
 
     if args.device is not None:
-        print(f'Current device {torch.cuda.current_device()} ({torch.cuda.get_device_name()})')
+        print(
+            f"Current device {torch.cuda.current_device()} ({torch.cuda.get_device_name()})"
+        )
         torch.cuda.set_device(args.device)
-        print(f'New device {torch.cuda.current_device()} ({torch.cuda.get_device_name()})')
+        print(
+            f"New device {torch.cuda.current_device()} ({torch.cuda.get_device_name()})"
+        )
 
-    for i in range(args.minQ, args.maxQ+1):
+    for i in range(args.minQ, args.maxQ + 1):
         validate_quality(i, args.model)
