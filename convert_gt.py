cbe60b9f8a3f1d0f093d77b9b5aee6dea13431bd
tgandor <tomasz.gandor@gmail.com>
2021-06-14 01:02:14 +0200

    convert_gt.py - extracting GT objects (box) from COCO dataset annotations.

---
+++ convert_gt.py
@@ -0,0 +1,40 @@
+from argparse import ArgumentParser
+from itertools import count
+import json
+from pathlib import Path
+
+from evaluation import _load_gt
+
+parser = ArgumentParser()
+parser.add_argument("--bbox", action="store_true")
+parser.add_argument("--verbose", "-v", action="store_true")
+parser.add_argument("--limit", "-l", type=int, default=0)
+parser.add_argument("--output", "-o", type=Path, default="gt_objects.json")
+parser.add_argument("--indent", type=int)
+args = parser.parse_args()
+
+gt = _load_gt("coco_2017_val", debug=0)
+
+result = []
+
+for new_id, obj in zip(count(1), gt.dataset["annotations"]):
+    obj["area"] = round(obj["area"], 1)
+    obj["gt_id"] = new_id
+    del obj["id"]
+    obj["category"] = gt.cats[obj["category_id"]]["name"]
+    del obj["category_id"]
+    if args.bbox:
+        obj["x"], obj["y"], obj["w"], obj["h"] = obj["bbox"]
+        del obj["bbox"]
+
+    if args.verbose:
+        print(obj)
+
+    result.append(obj)
+
+    if new_id == args.limit:
+        break
+
+with args.output.open("w") as jsf:
+    json.dump(result, jsf, indent=args.indent)
+    jsf.write("\n")
