d87fa8314b54d30d56e6b15e129719f3ca03e36b
tgandor <tomasz.gandor@gmail.com>
2021-06-20 20:08:37 +0200

    add entry_point for evaldets.visualization

--- setup.py
+++ setup.py
@@ -2,7 +2,13 @@ from setuptools import setup
 
 setup(
     name="urban_oculus",
-    version="0.0.3",
+    version="0.0.4",
     description="Misc utils for object detection and image compression.",
     packages=["evaldets", "jpeg", "uo"],
+    entry_points=dict(
+        console_scripts=[
+            'view_detections=evaldets.visualization:_main',
+        ]
+    )
+
 )
