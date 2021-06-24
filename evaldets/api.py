7b9424b9f4aa01e7437d1c516298b2ef233eab82
tgandor <tomasz.gandor@gmail.com>
2021-06-25 00:24:14 +0200

    verifying postprocessing - baseline tables
    2021-06-24-baseline-tables.ipynb - checking results, noticed a problem
    (TPR > 1... WAT?)
    evaldets/postprocess.py - make header optional, instead of commented
    out.

--- evaldets/api.py
+++ evaldets/api.py
@@ -1,5 +1,6 @@
 # flake8: noqa
 from .metrics import *
 from .names import Names
+from .postprocess import *
 from .results import DetectionResults
 from .visualization import *
--- evaldets/postprocess.py
+++ evaldets/postprocess.py
@@ -1,5 +1,5 @@
+import argparse
 import glob
-import sys
 
 from uo.utils import load
 
@@ -8,18 +8,22 @@ def load_rich_results(reval_dir):
     return [load(rr) for rr in sorted(glob.glob(f"{reval_dir}/*/rich_results.json"))]
 
 
-def baseline_table(reval_dir):
+def baseline_table(reval_dir, header=False):
     # previously as evaluate_coco.py side-effect
     metrics = load_rich_results(reval_dir)
-    # print(
-    #     r"Model & AP & mAP\tsub{.5} & mAP\tsub{.75} & AP\tsub{l} & AP\tsub{m}"
-    #     r" & AP\tsub{s} & TPR & PPV & TP & FP \\"
-    # )
-    # print(r"\midrule")
+
+    if header:
+        # \newcommand\tsub[1]{\textsubscript{#1}}
+        print(
+            r"Model & AP & mAP\tsub{.5} & mAP\tsub{.75} & AP\tsub{l} & AP\tsub{m}"
+            r" & AP\tsub{s} & TPR & PPV & TP & FP \\"
+        )
+        print(r"\midrule")
+
     for row in metrics:
-        row['precision'] *= 100
-        row['recall'] *= 100
-        row['model'] = row['model'].replace('_', r'\_')  # LaTeX excape
+        row["precision"] *= 100
+        row["recall"] *= 100
+        row["model"] = row["model"].replace("_", r"\_")  # LaTeX excape
         print(
             "{model} & {AP:.1f} & {AP50:.1f} & {AP75:.1f} & {APl:.1f} & {APm:.1f}"
             r" & {APs:.1f} & {recall:.1f} & {precision:.1f} & {tp:,} & {fp:,} \\".format(
@@ -28,5 +32,13 @@ def baseline_table(reval_dir):
         )
 
 
+def _main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("reval_dir")
+    parser.add_argument('--header', action='store_true')
+    args = parser.parse_args()
+    baseline_table(args.reval_dir, args.header)
+
+
 if __name__ == "__main__":
-    baseline_table(sys.argv[1])
+    _main()
