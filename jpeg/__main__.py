9a35707a73e1829e7abb8747b3279376768c01a0
tgandor <tomasz.gandor@gmail.com>
2021-09-24 19:27:46 +0200

    jpeg/__main__.py -- improve --show-qts (reorder).

--- jpeg/__main__.py
+++ jpeg/__main__.py
@@ -6,7 +6,9 @@ from .quantization.ijg_tables import Q_to_QT1, Q_to_QT2
 
 parser = argparse.ArgumentParser("")
 parser.add_argument("filenames", nargs="*")
-parser.add_argument("--show-qts", type=int)
+parser.add_argument(
+    "--show-qts", type=int, help="a subcommand: only print specified quality's QTs."
+)
 parser.add_argument("--reorder", "-r", action="store_true", help="render 2d QTs")
 args = parser.parse_args()
 
@@ -19,5 +21,12 @@ for filename in args.filenames:
     print(f"Recognized Q: {recognize_QT_quality(filename)}")
 
 if args.show_qts is not None:
-    print(Q_to_QT1[args.show_qts])
-    print(Q_to_QT2[args.show_qts])
+    q1 = Q_to_QT1[args.show_qts]
+    q2 = Q_to_QT2[args.show_qts]
+
+    if args.reorder:
+        q1 = pprint.pformat(reorder(q1))
+        q2 = pprint.pformat(reorder(q2))
+
+    print(q1)
+    print(q2)
