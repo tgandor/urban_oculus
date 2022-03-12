090507f7e3b50536c398aa5786d0b35672ff6f86
tgandor <tomasz.gandor@gmail.com>
2022-03-12 16:59:00 +0100

    snippets/degrade_train2017.sh -- add unpacking before.

--- snippets/degrade_train2017.sh
+++ snippets/degrade_train2017.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+if [ "$1" == "" ] ; then
+    echo "Usage: $0 <QUALITY>"
+fi
+
+if [ -f "$HOME/train2017.zip" ] ; then
+    mkdir -p "$HOME/datasets/coco/"
+    unzip -o "$HOME/train2017.zip" -d "$HOME/datasets/coco/"
+fi
+
 time find $HOME/datasets/coco/train2017/ -type f | parallel mogrify -quality $1
+
+echo "Testing..."
+pushd $HOME/datasets/coco/train2017/
+ls | head | xargs ~/meats/dates/jpeg_quality.sh
